f9981c5c0f174395f15069dd049125c95b696855
Rahul Sheth <rahul@texel.ai>
2023-11-28 12:10:33 -0600

    Dynamically configure LLVM hash

--- cmake/HunterConfig.cmake
+++ cmake/HunterConfig.cmake
@@ -1,7 +1,14 @@
+file(STRINGS "${CMAKE_CURRENT_LIST_DIR}/llvm-hash.txt" TRITON_LLVM_HASH LIMIT_COUNT 1)
+if(NOT EXISTS "${CMAKE_BINARY_DIR}/${TRITON_LLVM_HASH}.tar.gz")
+  file(DOWNLOAD "https://github.com/llvm/llvm-project/archive/${TRITON_LLVM_HASH}.tar.gz" "${CMAKE_BINARY_DIR}/${TRITON_LLVM_HASH}.tar.gz")
+endif()
+file(SHA1 "${CMAKE_BINARY_DIR}/${TRITON_LLVM_HASH}.tar.gz" TRITON_LLVM_BINARY_HASH)
+string(SUBSTRING "${TRITON_LLVM_HASH}" 0 8 TRITON_LLVM_SHORT_HASH)
+
 hunter_config(LLVM
-    VERSION 18.0.0-49af6502-triton
-    URL "https://github.com/llvm/llvm-project/archive/49af6502c6dcb4a7f7520178bd14df396f78240c.tar.gz"
-    SHA1 55f7883b1ea90f74ea8d7c4d3db30b3564c0f84c
+    VERSION "18.0.0-${TRITON_LLVM_SHORT_HASH}-triton"
+    URL "${CMAKE_BINARY_DIR}/${TRITON_LLVM_HASH}.tar.gz"
+    SHA1 "${TRITON_LLVM_BINARY_HASH}"
     CMAKE_ARGS
         LLVM_ENABLE_PROJECTS=clang;mlir
         LLVM_INSTALL_UTILS=ON
